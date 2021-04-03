5f1ff7adf0149d836ac5decda6ce64cc7728ddae
Svitlana Antonenko <szemlyanaya@gmail.com>
2021-04-04 00:03:39 +0300

    Dockerfile changed9

--- src/static/js/app.js
+++ src/static/js/app.js
@@ -53,7 +53,7 @@ function TodoListCard() {
         <React.Fragment>
             <AddItemForm onNewItem={onNewItem} />
             {items.length === 0 && (
-                <p className="text-center">You have no todo items yet!* Add one above!</p>
+                <p className="text-center">You have no todo items yet!! Add one above!</p>
             )}
             {items.map(item => (
                 <ItemDisplay
@@ -106,7 +106,7 @@ function AddItemForm({ onNewItem }) {
                         disabled={!newItem.length}
                         className={submitting ? 'disabled' : ''}
                     >
-                        {submitting ? 'Adding...' : 'Add'}
+                        {submitting ? 'Adding...' : 'Add2'}
                     </Button>
                 </InputGroup.Append>
             </InputGroup>
